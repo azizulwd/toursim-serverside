dc4336a6e08e055ca42c464bf01fd03c5a8b74f2
azizulwd <azizulislamgd@gmail.com>
2021-10-30 19:24:14 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const { MongoClient } = require('mongodb');
+
 const app = express();
 const port = 5000;
+
+
+const uri = "mongodb+srv://<username>:<password>@cluster0.iy859.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 app.get('/', (req, res)=>{
     res.send("Running Server");
 });
 
+// user: tixgig
+// pass: S2ljzKKPNF3tN64t
 
 app.listen(port, ()=>{
     console.log("Running port", port);
