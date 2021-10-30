3bb673e005c7df944fb653066b4d7d7a06dc5ada
azizulwd <azizulislamgd@gmail.com>
2021-10-30 20:48:53 +0600

    db secure

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
+require('dotenv').config();
 
 const app = express();
 const port = 5000;
 
 
-const uri = "mongodb+srv://<username>:<password>@cluster0.iy859.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iy859.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
@@ -13,8 +14,6 @@ app.get('/', (req, res)=>{
     res.send("Running Server");
 });
 
-// user: tixgig
-// pass: S2ljzKKPNF3tN64t
 
 app.listen(port, ()=>{
     console.log("Running port", port);
